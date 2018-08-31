49689b70f205bf3e74e348c558d68782e10524b6
Certil Remy <certil@noukod.com>
2018-08-31 18:36:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+
+def position_taken?(board, index)
+
+if board[index] == " " || ""
+  return false
+
+
+elsif board[index] == "X" || "O"
+  return true
+  
+end
+end
